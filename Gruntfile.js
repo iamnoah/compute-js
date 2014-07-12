dbb5c7428a6b98c99b4a397abe5a3e4cbf1d11c8
Noah Sloan <nsloan@spredfast.com>
2014-07-12 10:43:10 -0500

    moved code from mu

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+/*global module:false*/
+module.exports = function(grunt) {
+  "use strict";
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    simplemocha: {
+      options: {
+        globals: ['should'],
+        timeout: 3000,
+        ignoreLeaks: false,
+        ui: 'bdd',
+        reporter: 'tap'
+      },
+
+      all: { src: ['test/**/*.js'] }
+    },
+    browserify: {
+      compute: {
+        options: {
+          bundleOptions: {
+            standalone: "mu.compute",
+          },
+        },
+        src: "src/compute.js",
+        dest: "dist/compute.js"
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['simplemocha:all', "browserify:compute"]);
+
+};
